ac83d115701dfc3f4d1f215aae4cadd28092c8e2
Nitesh <niteshpurviya871@gmail.com>
2026-06-24 18:22:09 +0530

    Remove .env from repository

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import axios from 'axios'
 import Navbar from './components/navbar.jsx'
 import MidPart from './components/main_canvas.jsx'
 import BottomNav from './components/bottom_nav.jsx'
+
 import './App.css'
 const App = () => {
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
 
 const [weatherData, setWeatherData] = useState(null)
+const [city, setCity] = useState("Jaipur")
 
 useEffect(() => {
   const fetchWeather = async () => {
     try{
       const res = await axios.get(
-        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Jaipur&days=1`
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1`
       );
 
       setWeatherData(res.data);
@@ -26,11 +28,11 @@ useEffect(() => {
   };
 
   fetchWeather();
-},[]);
+},[city]);   // city change hote hi API call
 
   return (
     <div>
-      <Navbar />
+      <Navbar city={city} setCity={setCity}/>
       <MidPart weatherData={weatherData}/>
       <BottomNav />
     </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,59 @@
 import { Cloudy,Search } from 'lucide-react'
-const Navbar = () => {
+import { useState } from 'react'
+
+const Navbar = ({city,setCity}) => {
+  const [showSearch, setShowSearch] = useState(false)
+  const [inputValue, setInputValue] = useState("")
+
+  const handleSearch = () => {
+    if(inputValue.trim()) {
+      setCity(inputValue)
+      setInputValue("")
+    }
+  }
+
+
   return (
     <header className='fixed flex w-full h-16 z-50 border-b backdrop-blur-[20px] border-white/20 text-white items-center bg-white/10 justify-between px-6' >
+
       <div className='flex items-center gap-2' >
-        <span className='materials-symbols-outlines'><Cloudy /></span>
-        <span><h3 className='font-bold text-2xl tracking-tighter '> Weathers</h3></span>
+        
+        <span className='materials-symbols-outlines'><Cloudy />
+        </span>
+
+        <span>
+          <h3 className='font-bold text-2xl tracking-tighter '> 
+            Weathers
+          </h3>
+        </span>
+
       </div>
+
       <div className="flex items-center gap-4">
-        <button className='p-2 hover:bg-white/10 '>
+        {showSearch && (
+          <input
+            type="text"
+            placeholder="Search city..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearch();
+              }
+              }}
+            className="bg-white/10 border border-white/20 px-3 py-1 rounded-lg outline-none text-white"
+          />
+        )}
+
+        <button 
+          onClick={() => {
+            if (showSearch){
+              handleSearch();
+            } else{
+              setShowSearch(true)
+            }
+          }}
+        className='p-2 hover:bg-white/10 rounded-lg'>
           <span className='materials-symbols-outlines text-white'><Search /></span>
         </button>
       </div>
